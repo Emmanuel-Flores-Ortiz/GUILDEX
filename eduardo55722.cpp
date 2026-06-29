88fc645d90f0e8e23b5ef2986a9d4abaf01ececc
Eduardo Flores <eduardo.flores@uniat.edu.mx>
2026-06-29 12:12:42 -0700

    Código de prueba para PR

---
+++ eduardo55722.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+	float n1;
+	float n2;
+	std::cout << "Escribe dos números: " << std::endl;
+	std::cin >> n1 >> n2;
+	std::cout << "La suma es " << (n1 + n2) << std::endl;
+	return 0;
+}
